8f32a34b6118e1183177a8b5b43cb4a490f67e05
npe <npe@everymatrix.com>
2016-03-15 12:52:46 +0200

    1) added @AfterMethod annotation to quit webDriver in case of fail 2) added assertion for the test

---
+++ src/main/java/framework/BaseTest.java
@@ -0,0 +1,17 @@
+package framework;
+
+import org.testng.annotations.AfterMethod;
+
+import pages.BasePage;
+
+public class BaseTest extends BasePage {
+	
+	@AfterMethod
+	public void tearDown() {
+		if (SeleniumDriver.getInstance().getWebDriver() != null) {
+			SeleniumDriver.getInstance()
+					.quitSeleniumDriver();
+		}
+	}
+
+}
--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,28 +1,29 @@
+import org.testng.Assert;
+
 import framework.SeleniumDriver;
 import pages.BasePage;
 import pages.GoogleHomePage;
 
 public class SearchTest extends BasePage {
 	public static void main(String[] args) {
+		String searchPhrase = "Cheese!";
+		String expectedPageTitle = "Cheese! - Пошук Google";
+		String expectedPageTitlePartialText = "cheese";
+		String actualPagetitleBeforeSearch;
+		String actualPageTitleAfterSearch;
 		
 		GoogleHomePage googleHomePage = new GoogleHomePage();
 		googleHomePage.goToHomePage();
-		googleHomePage.searchByText("Cheese!");
+		googleHomePage.searchByText(searchPhrase);
+		
+		actualPagetitleBeforeSearch = googleHomePage.getPageTitle();
+		System.out.println("Page title before search is: " + actualPagetitleBeforeSearch);
 
-		System.out.println("Page title is: " + googleHomePage.getPageTitle());
+		googleHomePage.waitUntilPageTitleStartsWithPartialText(expectedPageTitlePartialText);
 
-		googleHomePage.waitUntilPageTitleStartsWithPartialText("cheese!");
-
-		// np: There is no assert in this test. At this point asserting must be
-		// done: do we receive expected page title
-		System.out.println("Page title is: " + googleHomePage.getPageTitle());
-
-		/*
-		 * This will be done a lot of times, so separate method must be created
-		 * for this But this doesn't guarantee that driver will quit if an
-		 * exception occurs. So @AfterMethod annotaion must be use to be sure
-		 * webBrowser will be closed after test failed
-		 */
+		actualPageTitleAfterSearch =  googleHomePage.getPageTitle();
+		System.out.println("Page after search is: " + actualPageTitleAfterSearch);
+		Assert.assertEquals(actualPageTitleAfterSearch, expectedPageTitle, "Actual page title is incorrect");
 
 		SeleniumDriver.getInstance().quitSeleniumDriver();
 	}
