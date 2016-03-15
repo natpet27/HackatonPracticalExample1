3da40f1319a607a41e5cd6209460ee8d3fca9f43
npe <npe@everymatrix.com>
2016-03-15 12:54:12 +0200

    added @Test annotation

--- src/test/java/SearchTest.java
+++ src/test/java/SearchTest.java
@@ -1,31 +1,33 @@
 import org.testng.Assert;
+import org.testng.annotations.Test;
 
 import framework.SeleniumDriver;
 import pages.BasePage;
 import pages.GoogleHomePage;
 
 public class SearchTest extends BasePage {
-	public static void main(String[] args) {
+
+	@Test
+	public void testSearchByPhrase() {
 		String searchPhrase = "Cheese!";
 		String expectedPageTitle = "Cheese! - Пошук Google";
 		String expectedPageTitlePartialText = "cheese";
 		String actualPagetitleBeforeSearch;
 		String actualPageTitleAfterSearch;
-		
+
 		GoogleHomePage googleHomePage = new GoogleHomePage();
 		googleHomePage.goToHomePage();
 		googleHomePage.searchByText(searchPhrase);
-		
+
 		actualPagetitleBeforeSearch = googleHomePage.getPageTitle();
 		System.out.println("Page title before search is: " + actualPagetitleBeforeSearch);
 
 		googleHomePage.waitUntilPageTitleStartsWithPartialText(expectedPageTitlePartialText);
 
-		actualPageTitleAfterSearch =  googleHomePage.getPageTitle();
+		actualPageTitleAfterSearch = googleHomePage.getPageTitle();
 		System.out.println("Page after search is: " + actualPageTitleAfterSearch);
 		Assert.assertEquals(actualPageTitleAfterSearch, expectedPageTitle, "Actual page title is incorrect");
 
 		SeleniumDriver.getInstance().quitSeleniumDriver();
 	}
-
 }
